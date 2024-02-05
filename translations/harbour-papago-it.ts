5c6ec8fbdefbedb5cf70fa43d1aff11b44c3d574
mkiol <michal@mkiol.net>
2024-02-05 12:49:00 +0100

    italian translation update (albanobattistella)

--- translations/harbour-papago-it.ts
+++ translations/harbour-papago-it.ts
@@ -1,42 +1,33 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="it">
 <context>
     <name>harbour-papago</name>
     <message>
-        <location filename="../qml/harbour-papago.qml" line="93"/>
-        <source>%1 listens</source>
-        <translation>%1 ascolta</translation>
+        <source>You can download Speech to Text, Text to Speech and Translator models for the languages you intend to use in the %1 app.</source>
+        <translation>Puoi scaricare i modelli di Speech to Text, sintesi vocale e traduttore per le lingue che intendi utilizzare nell&apos;app %1.</translation>
     </message>
     <message>
-        <location filename="../qml/harbour-papago.qml" line="125"/>
-        <source>%1 speeks</source>
-        <translation>%1 parla</translation>
-    </message>
-    <message>
-        <location filename="../qml/harbour-papago.qml" line="155"/>
-        <source>Confirm before speak</source>
-        <translation>Conferma prima di parlare</translation>
-    </message>
-    <message>
-        <location filename="../qml/harbour-papago.qml" line="159"/>
-        <source>No</source>
-        <translation>No</translation>
-    </message>
-    <message>
-        <location filename="../qml/harbour-papago.qml" line="162"/>
-        <source>Yes</source>
-        <translation>Si</translation>
-    </message>
-    <message>
-        <location filename="../qml/harbour-papago.qml" line="211"/>
         <source>No language is available.</source>
         <translation>Nessuna lingua è disponibile.</translation>
     </message>
     <message>
-        <location filename="../qml/harbour-papago.qml" line="212"/>
-        <source>You can download Speech to Text, Text to Speech and Translator models for the languages you intend to use in the %1 app.</source>
-        <translation type="unfinished">Puoi scaricare i modelli di sintesi vocale e traduttore per le lingue che intendi utilizzare nell'app %1.</translation>
+        <source>%1 listens</source>
+        <translation>%1 ascolta</translation>
+    </message>
+    <message>
+        <source>%1 speeks</source>
+        <translation>%1 parla</translation>
+    </message>
+    <message>
+        <source>Confirm before speak</source>
+        <translation>Conferma prima di parlare</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>No</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Si</translation>
     </message>
 </context>
 </TS>
